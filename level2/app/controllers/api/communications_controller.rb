127f5610c47ec628297c0fe6bc554585f377f48c
Florence Martino <florence.martino.fm@gmail.com>
2019-06-21 10:17:46 +0200

    change where with find_by in communication#create

--- level2/app/controllers/api/communications_controller.rb
+++ level2/app/controllers/api/communications_controller.rb
@@ -1,21 +1,14 @@
 class Api::CommunicationsController < ApplicationController
 
   def create
-    practitioner = Practitioner.where(first_name: communication_params[:first_name], last_name: communication_params[:last_name]).first
-
+    practitioner = Practitioner.find_by(first_name: communication_params[:first_name], last_name: communication_params[:last_name])
     communication = Communication.new(practitioner_id: practitioner.id, sent_at: communication_params[:sent_at])
-
     communication.save
-
     render json: communication.to_json
   end
 
   def index
-    #start_time = Time.now
     render json: Communication.all.includes(:practitioner).to_json
-    #end_time = Time.now
-    #time_final = end_time - start_time
-    #raise
   end
 
   def communication_params
