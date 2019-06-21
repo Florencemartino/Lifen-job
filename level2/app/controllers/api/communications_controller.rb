375df54c48d980f0a1482c835a6db24965aaa261
Florence Martino <florence.martino.fm@gmail.com>
2019-06-21 17:08:34 +0200

    add pagination for index method

--- level2/app/controllers/api/communications_controller.rb
+++ level2/app/controllers/api/communications_controller.rb
@@ -2,13 +2,17 @@ class Api::CommunicationsController < ApplicationController
 
   def create
     practitioner = Practitioner.find_by(first_name: communication_params[:first_name], last_name: communication_params[:last_name])
-    communication = Communication.new(practitioner_id: practitioner.id, sent_at: communication_params[:sent_at])
+    communication = Communication.new(practitioner: practitioner, sent_at: communication_params[:sent_at])
     communication.save
     render json: communication.to_json
   end
 
   def index
-    render json: Communication.all.includes(:practitioner).to_json
+    @communications =  Communication.all.paginate(:page => params[:page], :per_page => 30)
+      group_by_practitioner = @communications.group_by do |communication|
+         " #{communication.practitioner.first_name} #{communication.practitioner.last_name} "
+      end
+    render :json => group_by_practitioner.to_json
   end
 
   def communication_params
--- level2/app/models/communication.rb
+++ level2/app/models/communication.rb
@@ -3,8 +3,6 @@ class Communication < ApplicationRecord
 
   def as_json(options = nil)
     {
-      first_name: practitioner.first_name,
-      last_name: practitioner.last_name,
       sent_at: sent_at
     }
   end
